3bc849c27a2c1813a297ea16826aae9ece688c25
Nikita Perevalov <naperevalov@spasibosb.ru>
2025-08-06 12:02:49 +0300

    init commit

---
+++ src/main/java/ru/perevalov/gamerecommenderai/GameRecommenderAiApplication.java
@@ -0,0 +1,13 @@
+package ru.perevalov.gamerecommenderai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRecommenderAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameRecommenderAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/perevalov/gamerecommenderai/GameRecommenderAiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.perevalov.gamerecommenderai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRecommenderAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
