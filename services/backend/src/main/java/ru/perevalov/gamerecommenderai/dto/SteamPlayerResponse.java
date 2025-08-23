5ac3086b36f4666606d29d798ee764615b22ac33
Vladimir <kazimirov.2013@mail.ru>
2025-08-23 11:50:30 +0300

    Add: создан модуль SteamClient  с методами: getPlayerSummaries, getOwnedGames

---
+++ services/backend/src/main/java/ru/perevalov/gamerecommenderai/dto/SteamPlayerResponse.java
@@ -0,0 +1,28 @@
+package ru.perevalov.gamerecommenderai.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SteamPlayerResponse {
+    private Response response;
+
+    @Data
+    public static class Response {
+        private List<Player> players;
+    }
+
+    @Data
+    public static class Player {
+        @JsonProperty("steamid")
+        private String steamId;
+
+        @JsonProperty("personaname")
+        private String personaName;
+
+        @JsonProperty("avatarfull")
+        private String avatarFull;
+    }
+}
