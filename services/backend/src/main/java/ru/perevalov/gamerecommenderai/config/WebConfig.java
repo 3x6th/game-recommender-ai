d45e5740a0ac08d20c8b3baa99a9cf4c00b8236b
Nikita Perevalov <naperevalov@spasibosb.ru>
2025-10-13 02:02:16 +0300

    PCAI-38 [UI] Интеграция с backend

--- services/backend/src/main/java/ru/perevalov/gamerecommenderai/config/WebConfig.java
+++ services/backend/src/main/java/ru/perevalov/gamerecommenderai/config/WebConfig.java
@@ -1,19 +1,13 @@
 package ru.perevalov.gamerecommenderai.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.perevalov.gamerecommenderai.controller.resolver.OpenIdResponseMethodArgumentResolver;
 
-import java.util.List;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Value("${application.security.cors.allowed-origins}")
-    private String[] allowedOrigins;
 
     /**
      * Регистрируем новый MethodArgumentResolver в ApplicationContext
@@ -21,19 +15,5 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new OpenIdResponseMethodArgumentResolver());
-    }
-
-    /**
-     * Настройка CORS для фронтенда
-     */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins(allowedOrigins)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .exposedHeaders("Authorization")
-                .allowCredentials(true)
-                .maxAge(3600);
     }
 }
