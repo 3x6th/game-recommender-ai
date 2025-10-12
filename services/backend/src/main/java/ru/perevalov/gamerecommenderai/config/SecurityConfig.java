6c66900c51f03b944fc0f93bf5fb5bdcd9cd1af0
Nikita Perevalov <naperevalov@spasibosb.ru>
2025-10-13 01:48:31 +0300

    PCAI-38 [UI] Интеграция с backend

--- services/backend/src/main/java/ru/perevalov/gamerecommenderai/config/SecurityConfig.java
+++ services/backend/src/main/java/ru/perevalov/gamerecommenderai/config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configure(http))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
