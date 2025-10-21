04365b52b7ded3b82ba58279f28b6db08038d63c
Amirhan <122749296+Amirhan2@users.noreply.github.com>
2025-10-21 12:45:26 +0300

    PCAI-77: Миграция Maven зависимостей
    * PCAI-77: Миграция Maven зависимостей

--- services/backend/src/main/java/ru/perevalov/gamerecommenderai/GameRecommenderAiApplication.java
+++ services/backend/src/main/java/ru/perevalov/gamerecommenderai/GameRecommenderAiApplication.java
@@ -2,6 +2,9 @@ package ru.perevalov.gamerecommenderai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.r2dbc.R2dbcRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,7 +13,12 @@ import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+// TODO: Удалить exclude после завершения задачи PCAI-78
+@SpringBootApplication(exclude = {
+        R2dbcAutoConfiguration.class,
+        R2dbcDataAutoConfiguration.class,
+        R2dbcRepositoriesAutoConfiguration.class
+})
 @EnableConfigurationProperties
 @EnableCaching
 @EnableAspectJAutoProxy
