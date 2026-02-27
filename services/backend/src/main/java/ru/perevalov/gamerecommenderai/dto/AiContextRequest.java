ee653b7eb2a743ba48bcc862e9078bed206e9aaf
Edvard <edickk64@gmail.com>
2026-02-27 19:22:14 +0300

    Доработка, форматирование, удаление лишнего2

--- services/backend/src/main/java/ru/perevalov/gamerecommenderai/dto/AiContextRequest.java
+++ services/backend/src/main/java/ru/perevalov/gamerecommenderai/dto/AiContextRequest.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.perevalov.gamerecommenderai.dto.steam.SteamOwnedGamesResponse;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -13,5 +15,15 @@ import ru.perevalov.gamerecommenderai.dto.steam.SteamOwnedGamesResponse;
 public class AiContextRequest {
     private String userMessage;
     private String[] selectedTags;
-    private SteamOwnedGamesResponse gameLibrary;
+    private List<String> userSteamLibrary;
+
+    private String chatId;
+    private String agentId;
+    private String requestId;
+    private String correlationId;
+
+
+    private String language;
+    private List<String> excludeGenres;
+    private int maxResults;
 }
