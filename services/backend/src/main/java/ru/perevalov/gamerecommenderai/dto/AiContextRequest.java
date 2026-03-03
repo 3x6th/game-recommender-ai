696451d3950847f43c45b90e5011cb75ac50d8f1
Edvard <edickk64@gmail.com>
2026-03-03 15:50:07 +0300

    PCAI-120 Исправлена ошибка с билдером

--- services/backend/src/main/java/ru/perevalov/gamerecommenderai/dto/AiContextRequest.java
+++ services/backend/src/main/java/ru/perevalov/gamerecommenderai/dto/AiContextRequest.java
@@ -1,11 +1,12 @@
 package ru.perevalov.gamerecommenderai.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
